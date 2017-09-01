c0410a99836a047de35d0e9852befb249abf01f5
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-01 17:15:59 -0400

    First commit

---
+++ ScrollerTest/Entities/Tomato.swift
@@ -0,0 +1,18 @@
+//
+// Created by Jeffrey Wu on 2017-09-01.
+// Copyright (c) 2017 okAy Studios. All rights reserved.
+//
+
+import GameplayKit
+
+class Tomato: GKEntity
+{
+    override init()
+    {
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
