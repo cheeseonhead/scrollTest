73c6ae0f8a4a6a4d7c073d790ecfafb01e876c11
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-01 17:57:44 -0400

    Added entitymanager

---
+++ ScrollerTest/Entities/EntityManager.swift
@@ -0,0 +1,17 @@
+//
+// Created by Jeffrey Wu on 2017-09-01.
+// Copyright (c) 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class EntityManager
+{
+    weak var scene: SKScene?
+
+    init(scene: SKScene)
+    {
+        self.scene = scene
+    }
+}
