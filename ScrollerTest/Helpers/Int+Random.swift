f397446e39c4d5d9e2f1a9b43e4bd6173ee89b2f
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-01 18:53:31 -0400

    Added new util

---
+++ ScrollerTest/Helpers/Int+Random.swift
@@ -0,0 +1,16 @@
+//
+// Created by Jeffrey Wu on 2017-09-01.
+// Copyright (c) 2017 okAy Studios. All rights reserved.
+//
+
+import Foundation
+
+extension Int
+{
+    static func random(min: Int, max: Int) -> Int
+    {
+        assert(max > min)
+        let upperBound = UInt32(max - min)
+        return Int(arc4random_uniform(upperBound)) + min
+    }
+}
