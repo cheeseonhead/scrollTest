0e0fc76801848e58219afa0e39bac926465f3e9c
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-01 18:14:51 -0400

    Added rope

---
+++ ScrollerTest/Entities/Rope.swift
@@ -0,0 +1,24 @@
+//
+// Created by Jeffrey Wu on 2017-09-01.
+// Copyright (c) 2017 okAy Studios. All rights reserved.
+//
+
+import GameplayKit
+import SpriteKit
+
+class Rope: GKEntity
+{
+    override init()
+    {
+        super.init()
+
+        let texture = SKTexture(imageNamed: "Rope")
+        let spriteComponent = SpriteComponent(texture: texture)
+
+        addComponent(spriteComponent)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
