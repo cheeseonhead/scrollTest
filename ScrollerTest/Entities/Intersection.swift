d7ab1a71bd3223f0b65a87d1dace6b8c13583377
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-02 21:04:14 -0400

    Added intersection

---
+++ ScrollerTest/Entities/Intersection.swift
@@ -0,0 +1,19 @@
+//
+// Created by Jeffrey Wu on 2017-09-02.
+// Copyright (c) 2017 okAy Studios. All rights reserved.
+//
+
+import GameplayKit
+
+class Intersection: GKEntity
+{
+    init(position: CGPoint)
+    {
+        let intersectionComponent = IntersectionComponent(position: position)
+        addComponent(intersectionComponent)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
