1a4b5a2f1d242f609ca7bc903119eab6924cb941
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-03 13:15:01 -0400

    Added second move compoennt

---
+++ ScrollerTest/Components/MoveComponentV2.swift
@@ -0,0 +1,12 @@
+//
+// Created by Jeffrey Wu on 2017-09-03.
+// Copyright (c) 2017 okAy Studios. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class MoveComponentV2: GKComponent
+{
+
+}
