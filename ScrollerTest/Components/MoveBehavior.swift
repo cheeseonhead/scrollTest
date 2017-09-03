f57d99bcaac7367481801816fa7b79b77809c2fe
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-03 13:13:23 -0400

    Add target speed to the goals

--- ScrollerTest/Components/MoveBehavior.swift
+++ ScrollerTest/Components/MoveBehavior.swift
@@ -7,10 +7,13 @@ import GameplayKit
 
 class MoveBehavior: GKBehavior
 {
-    init(path: GKPath)
+    init(path: GKPath, targetSpeed: Float)
     {
         super.init()
 
-        setWeight(1.0, for: GKGoal(toFollow: path, maxPredictionTime: 1.0, forward: true))
+        if targetSpeed > 0 {
+            setWeight(0.5, for: GKGoal(toReachTargetSpeed: targetSpeed))
+            setWeight(1.0, for: GKGoal(toFollow: path, maxPredictionTime: 1, forward: true))
+        }
     }
 }
