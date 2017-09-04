5bb44e213770c60e0034a55366ca5d66ad9355db
Jeffrey Wu <cheeseonhead@gmail.com>
2017-09-04 13:37:46 -0400

    Just let the intersection component handle all intersections

--- ScrollerTest/Components/IntersectionComponent.swift
+++ ScrollerTest/Components/IntersectionComponent.swift
@@ -7,5 +7,23 @@ import GameplayKit
 
 class IntersectionComponent: GKComponent
 {
-    var position = CGPoint(x: 0, y: 0)
+    var intersections = [CGPoint(), CGPoint()]
+    var entityPosition = CGPoint(x: 0, y: 0)
+
+    override func update(deltaTime seconds: TimeInterval)
+    {
+        super.update(deltaTime: seconds)
+
+        if let spriteNode = entity?.component(ofType: SpriteComponent.self)?.node {
+
+            guard entityPosition != spriteNode.position else {return}
+
+            entityPosition = spriteNode.position
+
+            let frame = spriteNode.frame
+            let yPos = frame.origin.y + frame.size.height / 2
+            intersections[0] = CGPoint(x: frame.origin.x, y: yPos)
+            intersections[1] = CGPoint(x: frame.origin.x + frame.size.width, y: yPos)
+        }
+    }
 }
